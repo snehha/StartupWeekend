5a114de853f80c079b682971fd5ff5bcf100cd04
snehha <snehapradhan@hotmail.com>
2015-07-18 12:14:22 +0545

    Add form to the fancybox.

--- frontEnd/assets/js/main.js
+++ frontEnd/assets/js/main.js
@@ -1,13 +1,54 @@
 $(document).ready(function(){
 	$('#donate_txt').on('click', function(event){
 		event.preventDefault();
-		//notify afer submission
+		var $template = $('<form id="donation-form" action="#">\
+		        <div class="col-xs-12">\
+		          <div class= "form-group form-wrapper">\
+		            <div class="form-group">\
+		              <label for="first-name">First Name:</label>\
+		              <input id="first-name" type="text" class="form-control" placeholder="--First Name--"/>\
+		            </div>\
+		            <div class="form-group">\
+		              <label for="last-name">Last Name:</label>\
+		              <input id="last-name" type="text" class="form-control" placeholder="--Last Name--">\
+		            </div>\
+		            <div class="form-group">\
+		              <label for="email-id">Email-Id:</label>\
+		              <input id="email-id" type="text" class="form-control" placeholder="--Email-ID--">\
+		            </div>\
+		            <div class="form-group">\
+		              <label for="phone-no">Phone Number</label>\
+		              <input id="phone-no" type="text" class="form-control" placeholder="--Phone Number--">\
+		            </div>\
+		            <div class="form-group">\
+		              <label for="donation-amount">Donation Amount</label>\
+		              <input type="text" id="donation-amount" class="form-control" placeholder="--Donation Amount--">\
+		            </div>\
+		            <div class="col-xs-12">\
+		              <input type="submit" id="submit-btn" value="Submit" class=" btn floatR">\
+		            </div>\
+		          </div>\
+		        </div>\
+		      </form>')
+		
+		// var $donation_template = document.querySelector('#main-content').content;
+		
+		$.fancybox({
+            'content'          : $template
+        });
+        return false;
+	});
+
+	$(document).on('submit', '#donation-form' ,function(event){
+		event.preventDefault();
+		$.fancybox.close();
 		noty({
 			text: 'Thank you for donating your amount',
 			layout: 'bottomRight',
 			type: 'information',
 			timeout: 2000
 		});
-	})
 
-})
+	});
+
+});
